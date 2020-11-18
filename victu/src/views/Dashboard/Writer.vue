e514c6095a5a4de98d1443522b862df253d74129
verm024 <muhnaufal251@gmail.com>
2020-11-18 15:24:26 +0700

    Dashboard Writer

--- victu/src/views/Dashboard/Writer.vue
+++ victu/src/views/Dashboard/Writer.vue
@@ -5,7 +5,52 @@
 </template>
 
 <script>
-export default {};
+import firebase from "../../firebase";
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      daftar_konten: ""
+    };
+  },
+  watch: {
+    get_daftar_konten: {
+      immediate: true,
+      handler() {
+        this.$bind(
+          "daftar_konten",
+          firebase.db
+            .collection("contents")
+            .where(
+              "writer",
+              "==",
+              firebase.db.collection("users").doc(this.currentUser.uid)
+            )
+            .orderBy("tanggal_diposting", "desc")
+        );
+      }
+    }
+  },
+  computed: {
+    ...mapState(["currentUser", "userProfile"]),
+    filterProofreading() {
+      return this.daftar_konten.filter(element => {
+        return element.status == "proofreading";
+      });
+    },
+    filterPosted() {
+      return this.daftar_konten.filter(element => {
+        return element.status == "posted";
+      });
+    },
+    filterDeleted() {
+      return this.daftar_konten.filter(element => {
+        return element.status == "deleted";
+      });
+    }
+  }
+};
 </script>
 
 <style></style>
