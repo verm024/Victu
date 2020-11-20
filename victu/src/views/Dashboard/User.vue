29244e78f603b0bf4b805c161f5d209c3b19afd6
Aziz Anwar <azisanw19@gmail.com>
2020-11-20 08:35:19 +0700

    dashboard user

--- victu/src/views/Dashboard/User.vue
+++ victu/src/views/Dashboard/User.vue
@@ -1,7 +1,68 @@
 <template>
-  <div class="user">
-    Dashboard User
-    <br />
+  <div class="user d-flex align-center flex-column">
+    <div class="logo">
+      <img src="@/assets/images/logo.png" alt="Victu" />
+    </div>
+
+    <v-container class="profile d-flex" fluid>
+      <v-row>
+        <v-col class="col-6 col-md-6 offset-md-3">
+          <div class="profile-title">
+            Profile Information
+          </div>
+          <div class="d-flex align-center mb-6">
+            <v-avatar size="116" tile class="pl-2">
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"
+                alt="profile image"
+              />
+            </v-avatar>
+            <div class="ml-2">
+              <h4>Adam Jones</h4>
+              <h4>25 Years old</h4>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col class="col-6 col-md-6 offset-md-3">
+          <div class="profile-right-title">
+            Today's Date
+          </div>
+          <div class="profile-right-content">
+            18 November 2020
+          </div>
+          <div class="profile-right-title">
+            Today's Calorie Total
+          </div>
+          <div class="profile-right-content">
+            2300 calories
+          </div>
+          <div class="profile-right-title">
+            Last Calorie Intake
+          </div>
+          <div class="profile-right-content">
+            700 calories at 15:40
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container>
+      <div class="profile-title">
+        LATEST RECOMMENDATION
+      </div>
+      <v-card class="mx-auto my-12" max-width="150">
+        <v-img
+          height="100"
+          src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+        <div class="card-title">Ayam Goreng Rempah</div>
+        <div class="card-subtitle">Calorie: 500 cals</div>
+      </v-card>
+    </v-container>
     <router-link to="/search-content">
       Lihat konten kesehatan
     </router-link>
@@ -12,4 +73,62 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.user {
+  min-height: 100vh;
+  margin: 0;
+}
+
+.logo {
+  margin-bottom: 40px;
+}
+
+.logo img {
+  width: 150px;
+}
+
+.profile {
+  background: #f9edee;
+}
+
+.profile-title {
+  font-family: Roboto;
+  font-size: 14px;
+  color: #28190e;
+  padding: 0 8px;
+  margin-bottom: 3px;
+}
+
+.profile-right-title {
+  font-family: Roboto;
+  font-size: 14px;
+  color: #b5adad;
+  padding: 0 8px;
+}
+
+.profile-right-content {
+  font-family: Roboto;
+  font-size: 14px;
+  color: #28190e;
+  padding: 0 8px;
+  margin-bottom: 3px;
+}
+
+.card-title {
+  font-family: Roboto;
+  font-size: 14px;
+  color: #28190e;
+  padding: 0 8px;
+  margin-bottom: 3px;
+  margin-top: 3px;
+}
+
+.card-subtitle {
+  font-family: Roboto;
+  font-size: 12px;
+  color: #28190e;
+  padding: 0 8px;
+  margin-bottom: 3px;
+  margin-top: 3px;
+}
+</style>
