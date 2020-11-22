50d249a95b4686d01bdf76710bc4b433e3ad2fd8
verm024 <muhnaufal251@gmail.com>
2020-11-23 00:14:01 +0700

    Tampilan Daftar Proofreading

--- victu/src/views/ProofreadingContent.vue
+++ victu/src/views/ProofreadingContent.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="proofreading">
-    Daftar Konten yang harus diproofreading
-    <div v-for="(item, index) in daftar_konten" :key="index">
-      <router-link :to="'/content/' + item.id">
-        {{ item.judul }}
-        {{ item.isi }}
-        {{ item.id }}
-      </router-link>
+  <div class="proofreading container">
+    <div
+      class="card-proofreading"
+      v-for="(item, index) in daftar_konten"
+      :key="index"
+      @click="$router.push('/content/' + item.id)"
+    >
+      <div class="image-wrapper">
+        <div class="image">
+          
+          <v-img :src="item.foto"><v-overlay absolute></v-overlay></v-img>
+        </div>
+      </div>
+      <div class="content d-flex align-center">
+        <div class="judul">{{ item.judul }}</div>
+      </div>
+    </div>
+    <div v-if="daftar_konten.length == 0" class="empty">
+      No content yet
     </div>
   </div>
 </template>
@@ -41,4 +52,65 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.proofreading {
+  margin-top: 20px;
+}
+
+.card-proofreading {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  padding: 20px;
+  background: #ffffff;
+  border: 1px solid #f1f4f2;
+  box-sizing: border-box;
+  border-radius: 10px;
+  box-shadow: 2px 4px 30px rgba(247, 181, 22, 0.1);
+  margin-bottom: 20px;
+}
+
+.content {
+  margin-left: 20px;
+  width: 100%;
+}
+
+.image {
+  display: flex;
+  width: 50px;
+  height: 50px;
+  background: #f7b516;
+  border-radius: 100px;
+  justify-content: center;
+  overflow: hidden;
+}
+
+.judul {
+  color: #28190e;
+  font-weight: 600;
+  font-size: 18px;
+}
+
+.empty {
+  text-align: center;
+}
+
+.empty a span {
+  font-size: 16px;
+  color: #28190e;
+  opacity: 0.8;
+}
+
+.empty a u {
+  font-size: 16px;
+  color: #f7b516;
+  font-weight: 500 !important;
+}
+</style>
+
+<style>
+  .v-overlay__scrim {
+    background-color: #f7b516 !important;
+    opacity: .2 !important;
+  }
+</style>
