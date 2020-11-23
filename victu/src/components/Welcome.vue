973fccf594f09be1c2f25cb0e0ba41d3cbdc33ff
DelphicGen <gennardokidam@gmail.com>
2020-11-23 21:15:15 +0700

    HMM

--- victu/src/components/Welcome.vue
+++ victu/src/components/Welcome.vue
@@ -4,7 +4,7 @@
   >
     <div>
       <h3 class="text-h5 text-sm-h3 mb-2">
-        Welcome, <strong>{{ userProfile.nama.split(" ")[0] }}</strong>
+        Welcome, <strong>{{ user.nama.split(" ")[0] }}</strong>
       </h3>
       <!-- <p class="gray--text">{{ user.kalori_ideal }} calories today</p> -->
     </div>
--- victu/src/views/SearchContent.vue
+++ victu/src/views/SearchContent.vue
@@ -1,23 +1,35 @@
 <template>
-  <div class="search-nutritionist">
-    Daftar konten
-    <div v-for="(item, index) in daftar_konten" :key="index">
-      <router-link :to="'/content/' + item.id">
-        {{ item.judul }}
-        {{ item.isi }}
-        {{ item.id }}
-      </router-link>
+  <v-container>
+    <Welcome :user="user" />
+    <div class="search-nutritionist">
+      <SectionHeader header="Serve you with the best" />
+      <div v-for="(item, index) in daftar_konten" :key="index">
+        <router-link :to="'/content/' + item.id">
+          {{ item.judul }}
+          {{ item.isi }}
+          {{ item.id }}
+        </router-link>
+      </div>
     </div>
-  </div>
+  </v-container>
 </template>
 
 <script>
 import firebase from "../firebase";
 import { mapState } from "vuex";
+import Welcome from "../components/Welcome";
+import SectionHeader from "../components/SectionHeader";
 
 export default {
+  components: {
+    Welcome,
+    SectionHeader
+  },
   data() {
     return {
+      user: {
+        nama: "John"
+      },
       daftar_konten: []
     };
   },
