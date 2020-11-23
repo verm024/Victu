daf3e79de1a740576dadfc43d4a6459555154be3
DelphicGen <gennardokidam@gmail.com>
2020-11-23 09:24:50 +0700

    dashboard nutrisionist

---
+++ victu/src/components/Article.vue
@@ -0,0 +1,43 @@
+<template>
+    <slide class="mr-3">
+        <v-card class="mx-auto" max-width="300">
+            <img
+                class="article__image"
+                :src="require(`@/assets/images/${article.src}`)"
+                alt=""
+            />
+
+            <div class="pa-3">
+                <div class="card-title">
+                    <strong>{{ article.title }}</strong>
+                </div>
+                <div class="card-subtitle grey--text article__details">{{ article.details }}</div>
+            </div>
+        </v-card>
+    </slide>
+</template>
+
+<script>
+import { Slide } from "vue-carousel";
+export default {
+  props: ["article"],
+  components: {
+    Slide
+  }
+};
+</script>
+
+<style>
+.article__image {
+  width: 100%;
+  height: auto;
+}
+.article__detail {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    margin-bottom: 20px !important;
+}
+</style>
--- victu/src/views/Dashboard/Nutritionist.vue
+++ victu/src/views/Dashboard/Nutritionist.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="nutritionist">
+  <!-- <div class="nutritionist">
     Dashboard Nutritionist
     <br />
     <router-link to="/chat">
@@ -9,11 +9,93 @@
     <router-link to="/proofreading">
       Content butuh proofreading
     </router-link>
-  </div>
+  </div> -->
+  <v-container>
+    <Welcome />
+
+    <div class="mb-15">
+      <SectionHeader header="Requested" />
+      <carousel
+        :perPageCustom="[
+          [330, 1],
+          [560, 2],
+          [768, 3]
+        ]"
+        :navigationEnabled="true"
+      >
+        
+        <Article v-for="(article, i) in requestedArticles" :key="i" article="article" />
+      </carousel>
+    </div>
+<!-- 
+    <div class="mb-15">
+      <SectionHeader header="Published" />
+      <carousel
+        :perPageCustom="[
+          [330, 1],
+          [560, 2],
+          [768, 3]
+        ]"
+        :navigationEnabled="true"
+      >
+        <Article v-for="(article, i) in publishedArticles" :key="i" article="article" />
+      </carousel>
+    </div> -->
+    
+  </v-container>
 </template>
 
 <script>
-export default {};
+import SectionHeader from "../../components/SectionHeader";
+import Welcome from "../../components/Welcome";
+import Article from "../../components/Article";
+import { Carousel } from "vue-carousel";
+export default {
+  components: {
+    SectionHeader,
+    Welcome,
+    Article,
+    Carousel
+  },
+  data() {
+    return {
+      requestedArticles: [
+        {
+          title: "Sehat tapi Enak, Apa itu?",
+          details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita illo at dignissimos unde, omnis nemo maxime possimus laudantium eos odio, animi blanditiis odit! Placeat aperiam voluptatem iure quia? Qui, minus.",
+          src: "food.png"
+        },
+        {
+          title: "Sehat tapi Enak, Apa itu?",
+          details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita illo at dignissimos unde, omnis nemo maxime possimus laudantium eos odio, animi blanditiis odit! Placeat aperiam voluptatem iure quia? Qui, minus.",
+          src: "food.png"
+        },
+        {
+          title: "Sehat tapi Enak, Apa itu?",
+          details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita illo at dignissimos unde, omnis nemo maxime possimus laudantium eos odio, animi blanditiis odit! Placeat aperiam voluptatem iure quia? Qui, minus.",
+          src: "food.png"
+        },
+      ],
+      publishedArticles: [
+        {
+          title: "Makan Oatmeal Secara Rutin",
+          details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita illo at dignissimos unde, omnis nemo maxime possimus laudantium eos odio, animi blanditiis odit! Placeat aperiam voluptatem iure quia? Qui, minus.",
+          src: "food.png"
+        },
+        {
+          title: "Makan Oatmeal Secara Rutin",
+          details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita illo at dignissimos unde, omnis nemo maxime possimus laudantium eos odio, animi blanditiis odit! Placeat aperiam voluptatem iure quia? Qui, minus.",
+          src: "food.png"
+        },
+        {
+          title: "Makan Oatmeal Secara Rutin",
+          details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita illo at dignissimos unde, omnis nemo maxime possimus laudantium eos odio, animi blanditiis odit! Placeat aperiam voluptatem iure quia? Qui, minus.",
+          src: "food.png"
+        },
+      ]
+    }
+  }
+};
 </script>
 
 <style></style>
